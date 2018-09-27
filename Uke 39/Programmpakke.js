6abf016f4fa780ba077cd37c9ab4d1952e45e2e5
Reonrallert <42641259+Reonrallert@users.noreply.github.com>
2018-09-27 12:33:35 +0200

    Update Programmpakke.js

--- Uke 39/Programmpakke.js
+++ Uke 39/Programmpakke.js
@@ -8,15 +8,15 @@ function vis(){
     var ant = document.getElementById("ant").value;
     console.log("Sum fungerer")
     if(ant<10 && ant>=0){
-        document.getElementById("result").innerHTML="Ingen rabbat, pris er: $" + 99;
+        document.getElementById("result").innerHTML="Ingen rabbat, pris er: $" + ant*99;
     }else if(ant<19 && ant>=10){
-        document.getElementById("result").innerHTML="10%, pris er: $" + (99*0.9).toFixed(2);
+        document.getElementById("result").innerHTML="10%, pris er: $" + (ant*99*0.9).toFixed(2);
     }else if(ant<49 && ant>=20){
-        document.getElementById("result").innerHTML="20%, pris er: $" + (99*0.8).toFixed(2);
-    }else if(ant<99 && ant>=50){
-        document.getElementById("result"). innerHTML="30%, pris er: $" + (99*0.7).toFixed(2);
+        document.getElementById("result").innerHTML="20%, pris er: $" + (ant*99*0.8).toFixed(2);
+    }else if(ant<100 && ant>=50){
+        document.getElementById("result"). innerHTML="30%, pris er: $" + (ant*99*0.7).toFixed(2);
     }else if(ant>=100){
-        document.getElementById("result").innerHTML="40%, pris er: $" + (99*0.6).toFixed(2);
+        document.getElementById("result").innerHTML="40%, pris er: $" + (ant*99*0.6).toFixed(2);
     }
                                                                     //toFixed(2) begrenser til 2 desimaler 
 }
